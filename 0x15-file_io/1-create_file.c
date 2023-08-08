eff3d6da71a6b6b4535dc06561f5bc2c2468fd88
HFMD04 <hafsamiad2021@gmail.com>
2023-08-08 15:16:35 -0700

    task1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,24 +9,24 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-        int fd, bytes, len = 0;
+int fd, bytes, len = 0;
 
-        if (filename == NULL)
-                return (-1);
+if (filename == NULL)
+return (-1);
 
-        if (text_content != NULL)
-        {
-                for (len = 0; text_content[len];)
-                        len++;
-        }
+if (text_content != NULL)
+{
+for (len = 0; text_content[len];)
+len++;
+}
 
-        fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-        bytes = write(fd, text_content, len);
+fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+bytes = write(fd, text_content, len);
 
-        if (fd == -1 || bytes == -1)
-                return (-1);
+if (fd == -1 || bytes == -1)
+return (-1);
 
-        close(fd);
+close(fd);
 
-        return (1);
+return (1);
 }
